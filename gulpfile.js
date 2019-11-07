5f93ed716653cb09061669f9afe3c2a0de815250
Vlad <curator.zog@gmail.com>
2019-11-07 20:02:05 +0200

    Add slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
 const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
 
 // Static server
 gulp.task("server", function() {
@@ -13,15 +15,18 @@ gulp.task("server", function() {
 
 gulp.task("styles", function() {
   return gulp
-    .src("src/sass/*.+(scss|sass)")
+    .src("src/sass/**/*.+(scss|sass)")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename({suffix: ".min", prefix: ""}))
+    .pipe(autoprefixer())
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function() {
-  gulp.watch("src/sass/.+(scss|sass)", gulp.parallel("styles"));
-  gulp.watch("src/.html").on("change", browserSync.reload);
+  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+  gulp.watch("src/js/script.js").on("change", browserSync.reload);
+  gulp.watch("src/index.html").on("change", browserSync.reload);
 });
 
 gulp.task("default", gulp.parallel("watch", "server", "styles"));
